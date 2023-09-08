ee3b93ff25519bb6cbc52ea0a8838ea614837fbf
root <root@WelcomeFuture>
2023-09-08 07:25:38 +0700

    versi 1

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+import Typed from 'typed.js';
+import confetti from 'canvas-confetti';
+
+new Typed('#heading', {
+    strings: ["Hey there!", "I'm Amru..😎", "Programming learner", "Nice to meet you 😁"],
+    typeSpeed: 78,
+    backSpeed: 98,
+    cursorChar: "_",
+    loop: true,
+  });
+
+const form = document.getElementById("form");
+
+form.addEventListener("keydown", (e)=>{
+  if(e.key === "Enter") {
+    const myCanvas = document.createElement("canvas");
+    document.body.appendChild(myCanvas);
+    confetti.create(myCanvas, {
+      resize: true,
+      useWorker: true
+    });
+    confetti({
+      particleCount: 1000,
+      spread: 300
+    });
+    myCanvas.remove();
+  }
+
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Epiloque", "sans-serif"],
+        display: ["Pacifico", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
